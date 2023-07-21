4733374822f99aeb27687d5e50a0cbc9d85c87f5
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-07-21 18:59:21 +0300

    Added email verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,10 @@
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { HttpError, ctrlWrapper } from "../helpers/index.js";
 import { User } from "../models/user.js";
 import { Word } from "../models/word.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const { SECRET_KEY } = process.env;
+import { sendEmail } from "../services/sendEmail.js";
+import { nanoid } from "nanoid";
+import tokenSetvice from "../services/tokens.js";
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -17,25 +15,26 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
-  const { _id } = newUser;
-  const token = jwt.sign({ id: _id }, SECRET_KEY, { expiresIn: "10h" });
+  const verificationCode = nanoid();
+  const varifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="https://tsylepa.github.io/Yummy/verification/${verificationCode}" >Click verify email</a>`,
+  };
 
-  await User.findByIdAndUpdate(_id, { token });
+  await sendEmail(varifyEmail);
+  const newUser = await User.create({ ...req.body, password: hashPassword, verificationCode });
+  const { _id } = newUser;
+
   await Word.create({
-    _id,
+    user: _id,
     vocabulary: [],
     firstLvl: [],
     secondLvl: [],
     thirdLvl: [],
   });
 
-  res.status(201).json({
-    token: `Bearer ${token}`,
-    user: {
-      email,
-    },
-  });
+  res.status(201).json("Verify your email to complete the registration");
 };
 
 const login = async (req, res) => {
@@ -50,24 +49,95 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password invalid");
   }
 
-  const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: "10h" });
-  await User.findByIdAndUpdate(user._id, { token });
+  const tokens = tokenSetvice.generateTokens({ id: _id, email: user.email });
+  await tokenSetvice.saveToken(_id, tokens.refreshToken);
+
+  res.cookie("refreshToken", tokens.refreshToken, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  });
 
   res.json({
-    token: `Bearer ${token}`,
+    ...tokens,
     user: {
       email,
     },
   });
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationCode } = req.params;
+  const findUser = await User.findOne({ verificationCode });
+  if (!findUser) {
+    throw HttpError(401, "User not found");
+  }
+  if (findUser.verifiedEmail) {
+    throw HttpError(401, "Email has already been verified");
+  }
+
+  const tokens = tokenSetvice.generateTokens({ id: _id, email: findUser.email });
+
+  await tokenSetvice.saveToken(_id, tokens.refreshToken);
+  await User.findByIdAndUpdate(findUser._id, {
+    verifiedEmail: true,
+    verificationCode: "",
+  });
+
+  res.cookie("refreshToken", tokens.refreshToken, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    ...tokens,
+    user: {
+      email: findUser.email,
+    },
+  });
+};
+
+const refreshToken = async (req, res) => {
+  const { refreshToken } = req.cookies;
+  if (!refreshToken) {
+    throw HttpError(403, "Token invalid");
+  }
+
+  const userData = tokenService.validateRefreshToken(refreshToken);
+  const tokenFromDb = await tokenService.findToken(refreshToken);
+  if (!userData || !tokenFromDb) {
+    throw HttpError(403, "Token invalid");
+  }
+
+  const tokens = tokenSetvice.generateTokens({ id: _id });
+  await tokenSetvice.saveToken(_id, tokens.refreshToken);
+
+  res.cookie("refreshToken", tokens.refreshToken, {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    ...tokens,
+    user: {
+      email: userData.email,
+    },
+  });
+};
+
 const getCurrent = async (req, res) => {
-  res.json(req.user);
+  const { email } = req.user;
+  res.json({
+    user: {
+      email,
+    },
+  });
 };
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
+  const { refreshToken } = req.cookies;
+  await tokenService.removeToken(refreshToken);
+  res.clearCookie("refreshToken");
+
   res.json({
     message: "Logout success",
   });
@@ -76,6 +146,8 @@ const logout = async (req, res) => {
 export const ctrl = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  verifyEmail: ctrlWrapper(verifyEmail),
+  refreshToken: ctrlWrapper(refreshToken),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
 };
