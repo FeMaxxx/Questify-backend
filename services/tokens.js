6035caa5709c95d6906432405e5b7bb3397de2f9
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-08-10 20:58:59 +0300

    Changed the lifetime of tokens

--- services/tokens.js
+++ services/tokens.js
@@ -7,8 +7,8 @@ const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = process.env;
 
 class TokenService {
   generateTokens(payload) {
-    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "10d" });
-    const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, { expiresIn: "10d" });
+    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "30m" });
+    const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, { expiresIn: "30d" });
     return {
       accessToken,
       refreshToken,
